fafdb5960ff5367e51a51ffdc9abe3d71e0073ae
Margarita <margarita27011991@gmail.com>
2022-10-03 17:18:48 +0300

    add homework3

--- Homework3/Task01.py
+++ Homework3/Task01.py
@@ -3,13 +3,13 @@
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
 from random import randint
-lst1 = [randint(-10, 10) for i in range(10)]
-lst2 = []
+list1 = [randint(-10, 10) for i in range(10)]
+list2 = []
 amount = 0
-print (lst1)
-for i in range(len(lst1)):
+print (list1)
+for i in range(len(list1)):
     if i%2 != 0:
-        lst2.append(lst1[i])
-        amount += lst1[i]
-print(lst2)
+        list2.append(list1[i])
+        amount += list1[i]
+print(list2)
 print(amount)
--- Homework3/Task02.py
+++ Homework3/Task02.py
@@ -2,3 +2,11 @@
 # *Пример:*
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+from random import randint
+list1 = [randint(0, 10) for i in range(7)]
+print (list1)
+list2 = []
+for i in range((len(list1)+1)//2):
+    list2.append(list1[i]*list1[len(list1)-1-i])
+print(list2)
