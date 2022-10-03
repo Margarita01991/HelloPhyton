304abd6d3f2611f7fc382a857a6d1083a5fce46d
Margarita <margarita27011991@gmail.com>
2022-10-03 15:41:22 +0300

    add homework2

--- Homework2/Task05.py
+++ Homework2/Task05.py
@@ -1,7 +1,8 @@
 # 5. Реализуйте алгоритм перемешивания списка.
 
-list = [14, 'desert', 24.1, 'home', 0.12, 'three']
-print(list) 
+# list = [14, 'desert', 24.1, 'home', 0.12, 'three']
 import random
+list = [i for i in range(random.randint(3,6))]
+print(list) 
 random.shuffle(list)
 print(list) 
