fe8b81df8b9d69622cb0120c893209a9e7cfc108
Margarita <margarita27011991@gmail.com>
2022-10-03 15:19:47 +0300

    add homework2

--- Homework2/Task03.py
+++ Homework2/Task03.py
@@ -1,9 +1,13 @@
 # Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
 # *Пример:*
 # - Для n = 6: [2.0, 2.25, 2.37037037037037, 2.44140625, 2.4883199999999994, 2.5216263717421135]
+import math
+n = int(input('Введите число: '))
+mass = [(1+1/i)**i for i in range(1,n+1)] 
+print(mass)
+print(sum(mass))
 
-n = int(input('Enter number: ')) 
-def list(n):
-    return[round((1 + 1 / number)**number, 16) for number in range (1, n + 1)]       
-print(list(n))
-print(round(sum(list(n)), 2))
+# def list(n):
+#     return[round((1 + 1 / number)**number, 16) for number in range (1, n + 1)]       
+# print(list(n))
+# print(round(sum(list(n)), 2))
