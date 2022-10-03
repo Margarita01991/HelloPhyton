137c2501c985813eee1a542bff10bdb9e1ed3db7
Margarita <margarita27011991@gmail.com>
2022-10-03 16:29:35 +0300

    add sem3

--- Sem3/Task02.py
+++ Sem3/Task02.py
@@ -1,2 +1,13 @@
 # 2) Задайте список. Напишите программу, которая определит, присутствует ли в заданном списке строк некое число.
 # [''ffff'.'3rfhg','4'] -> YES
+
+mass = ['ffff','3rfhg','4']
+result = 'No'
+for i in mass:
+    try:         # проверка 
+        int(i)
+        result = 'Yes'
+        break
+    except:
+        pass
+print(result)
