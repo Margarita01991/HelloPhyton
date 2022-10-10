61f855f658ea77ee2b71fe8b626ad72b332b27fb
Margarita <margarita27011991@gmail.com>
2022-10-10 19:15:43 +0300

    add sem5

--- Sem5/Task02.py
+++ Sem5/Task02.py
@@ -2,3 +2,15 @@
 # Порядок элементов менять нельзя.
 # Пример:
 # [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 6, 7] и т.д.
+num = [11, 5, 2, 3, 4, 6, 1, 7, 9, 8, 10]
+def nextmax(lst):    
+    max = lst[0]
+    res = [lst[0]]
+    for i in range(len(lst)):
+        if lst[i] > max:
+            max = lst[i]
+            res.append(max)
+    if len(res) == 1:
+        nextmax(lst[1:])
+    return res
+print(nextmax(num))
